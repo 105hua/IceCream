729fe0bc9975f961cf2368f6dfa2e55bd81c6230
105hua <joshuajtudball2003@gmail.com>
2024-09-15 02:10:24 +0100

    Added gradle dependencies.

--- src/main/java/com/nearvanilla/iceCream/IceCream.java
+++ src/main/java/com/nearvanilla/iceCream/IceCream.java
@@ -1,17 +1,18 @@
+/* Licensed under the GNU Affero General Public License. */
 package com.nearvanilla.iceCream;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class IceCream extends JavaPlugin {
 
-    @Override
-    public void onEnable() {
-        // Plugin startup logic
+  @Override
+  public void onEnable() {
+    // Plugin startup logic
 
-    }
+  }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
+  @Override
+  public void onDisable() {
+    // Plugin shutdown logic
+  }
 }
